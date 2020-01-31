51c05d450bd06f9899483f87030d55b832f0a022
Lydia Pierce <Lydia.Pierce@principia.edu>
2020-01-31 16:03:56 -0600

    Final version of Mod program. Includes 2 ways of finding the mod.

--- Day2_ModProgram/Day2_ModProgram.cpp
+++ Day2_ModProgram/Day2_ModProgram.cpp
@@ -5,14 +5,19 @@ Day2_ModProgram.cpp
 Manually create a Mod function for use
 
 Lydia Pierce
-v1.0 1/29/2020
+v1.0 1/31/2020
 */
 
 #include <iostream>
 
 
 
-//Get the min value of two input values
+/// <summary>
+/// Get the min value of two input values.
+/// </summary>
+/// <param name="valueA"></param>
+/// <param name="valueB"></param>
+/// <returns></returns>
 int GetMinValue(int valueA, int valueB)
 {
 	if (valueA < valueB)
@@ -21,12 +26,21 @@ int GetMinValue(int valueA, int valueB)
 		return valueB;
 }
 
+/// <summary>
+/// This function finds the remainder after dividing two values.
+/// </summary>
+/// <param name="valA"></param>
+/// <param name="valB"></param>
+/// <returns></returns>
 int Mod1(int valA, int valB)
 {
+	// If the divisor is less than valA, it divides it 0 times, and the reminder is the divisor.
 	if (valA < valB)
 		return valB;
+	// If the divisor is 0, it is undefined and returns a -1
 	else if (valB == 0)
 		return -1;
+	// Subtract valB from valA repeatedly until valA is smaller than valA. Then return valA, and that is the remainder.
 	else
 	{
 		while (valA >= valB)
@@ -37,12 +51,21 @@ int Mod1(int valA, int valB)
 	}
 }
 
+/// <summary>
+/// This function finds the reminder when value1 is divided by value2.
+/// </summary>
+/// <param name="value1"></param>
+/// <param name="value2"></param>
+/// <returns></returns>
 int Mod2(int value1, int value2)
 {
+	// If the divisor is greater than value1, the remainder is the divisor.
 	if (value1 < value2)
 		return value2;
+	// If the divisor is zero, it is undefined and returns -1 to print a different message.
 	else if (value2 == 0)
 		return -1;
+	// value2 divides value1 a certain number of times. That number is multiplied by value2 and subtracted from value1 to get the remainder.
 	else
 	{
 		int divide = value1 / value2;
@@ -55,24 +78,24 @@ int main()
 {
 	//    std::cout << "Hello World!\n";
 
-	int a = 19;
-	int b = 19;
+	int A = 400;
+	int b = 220;
 
-	// Find the minimum value and pretty print it to the screen
-	std::cout << "Min Value: " << GetMinValue(a, b) << "\n";
+	// Find the minimum value and pretty print it to the screen.
+	std::cout << "Min Value: " << GetMinValue(A, b) << "\n";
 
-	// Find the remainder when a is divided by b and pretty print it to the screen
-	if (Mod1(a, b) + Mod2(a, b) >= 0)
+	// Find the remainder when a is divided by b and pretty print it to the screen.
+	if (Mod1(A, b) + Mod2(A, b) > -1)
 	{
-		std::cout << "According to Mod1, the remainder when " << a << " is divided by " << b << " is " << Mod1(a, b) << ".\n";
-		std::cout << "According to Mod2, the remainder when " << a << " is divided by " << b << " is " << Mod2(a, b) << ".\n";
+		std::cout << "According to Mod1, the remainder when " << A << " is divided by " << b << " is " << Mod1(A, b) << ".\n";
+		std::cout << "According to Mod2, the remainder when " << A << " is divided by " << b << " is " << Mod2(A, b) << ".\n";
 	}
+	// In the case where the divisor is zero, it returns as undefined.
 	else 
 	{
 		std::cout << "Mod1 and Mod2 are undefined when b is " << b << ".\n";
 	}
 }
-
 
 
 
